07413e019c6a60d700be080ab8ca484478dce2a0
Dan Guest <dguest@cern.ch>
2015-07-24 00:02:57 -0700

    make chunking wrapper

--- include/tools.hh
+++ include/tools.hh
@@ -9,9 +9,60 @@ template<typename T>
 class OneDimBuffer
 {
 public:
-  OneDimBuffer(std::string ds_name, const H5::DataType& type, int max = 10);
+  OneDimBuffer(H5::CommonFG& group, std::string ds_name,
+	       H5::DataType type, hsize_t = 10);
+  void push_back(T new_entry);
+  void flush();
 private:
+  H5::DataType _type;
+  hsize_t _max_size;
+  hsize_t _offset;
   std::vector<T> _buffer;
+  H5::DataSet _ds;
 };
+
+template<typename T>
+OneDimBuffer<T>::OneDimBuffer(
+  H5::CommonFG& group, std::string ds_name,
+  H5::DataType type, hsize_t size):
+  _type(type),
+  _max_size(size),
+  _offset(0)
+{
+  hsize_t initial[1] = {0};
+  hsize_t eventual[1] = {H5S_UNLIMITED};
+
+  H5::DataSpace orig_space(1, initial, eventual);
+
+  H5::DSetCreatPropList params;
+  hsize_t chunk_size[1] = {100};
+  params.setChunk(1, chunk_size);
+
+  _ds = group.createDataSet(ds_name, _type, orig_space, params);
+}
+
+template<typename T>
+void OneDimBuffer<T>::push_back(T new_entry) {
+  if (_buffer.size() == _max_size) {
+    flush();
+  }
+  _buffer.push_back(new_entry);
+}
+template<typename T>
+void OneDimBuffer<T>::flush() {
+  if (_buffer.size() == 0) return;
+  hsize_t slab_dims[1] = {_buffer.size()};
+  hsize_t total_dims[1] = {slab_dims[0] + _offset};
+  _ds.extend(total_dims);
+  H5::DataSpace file_space = _ds.getSpace();
+  H5::DataSpace mem_space(1, slab_dims);
+  hsize_t offset_dims[1] = {_offset};
+  // std::cout << "selecting" << std::endl;
+  file_space.selectHyperslab(H5S_SELECT_SET, slab_dims, offset_dims);
+  // std::cout << "writing" << std::endl;
+  _ds.write(_buffer.data(), _type, mem_space, file_space);
+  _offset += _buffer.size();
+  _buffer.clear();
+}
 
 #endif
--- src/test-chunks.cxx
+++ src/test-chunks.cxx
@@ -1,3 +1,5 @@
+#include "tools.hh"
+
 #include <vector>
 #include <iostream>
 #include "H5Cpp.h"
@@ -24,9 +26,11 @@ int main(int argc, char* argv[]) {
   hsize_t max_size = 15;
   std::vector<int> buffer;
   hsize_t offset = 0;
+  OneDimBuffer<int> other_buf(file, "other_data", dtype);
   for (int iii = 0; iii < 100; iii++) {
     std::cout << "pushing " << iii << std::endl;
     buffer.push_back(iii);
+    other_buf.push_back(iii * 4);
     if (buffer.size() == max_size) {
       hsize_t slab_dims[1] = {buffer.size()};
       hsize_t total_dims[1] = {slab_dims[0] + offset};
