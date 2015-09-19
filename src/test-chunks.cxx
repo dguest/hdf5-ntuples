2978d341668311f3c72d2626836e8a57aef4c6bb
Dan Guest <dguest@cern.ch>
2015-09-19 02:44:23 -0700

    remove cruft

--- src/test-chunks.cxx
+++ src/test-chunks.cxx
@@ -35,23 +35,7 @@ struct MultiTrack {
 
 // This dummy `Entry` structure could correspond to an event, a jet, etc
 struct Entry {
-  double value_d;
-  int value_i;
-
-  // Variable length containers have to be stored in special vectors.
-  // Internally these wrap a hvl_t structure.
-  //
-  // Note the `h5` (lowercase) namespace: these are deinfed in
-  // `h5container.hh`, and should not be confused with stuff in the
-  // `H5` (uppercase) namespace which is provided in the official
-  // `H5Cpp.h` header.
-  h5::string value_s;
-  h5::vector<double> vector_d;
-  h5::vector<h5::string> vector_s;
-  h5::vector<h5::vector<int> > vv_i;
-  h5::vector<Track> tracks;
   h5::vector<MultiTrack> verts;
-
 };
 
 // _________________________________________________________________________
@@ -65,15 +49,6 @@ H5::CompType getEntryType() {
   // `itype` and `dtype` should be pretty straightforward.
   auto itype = H5::PredType::NATIVE_INT;
   auto dtype = H5::PredType::NATIVE_DOUBLE;
-  // `stype` is a string.
-  auto stype = H5::StrType(H5::PredType::C_S1, H5T_VARIABLE);
-  // `vl_dtype` is a variable-length double.
-  // The `VarLenType` constructor takes a _pointer_ to the base datatype.
-  auto vl_dtype = H5::VarLenType(&dtype);
-  auto vl_itype = H5::VarLenType(&itype);
-  auto vl_stype = H5::VarLenType(&stype);
-  // We can do as many levels of nesting as we want.
-  auto vvl_itype = H5::VarLenType(&vl_itype);
 
   // We're more interested in storing compound types. These can
   // contain any collection of int, float, strings, or `hvl_t`
@@ -137,10 +112,6 @@ int main(int argc, char* argv[]) {
   // The output buffer is periodically flushed.
   const size_t buffer_size = 100;
 
-  // Instance one example buffer. We'll call this one `ints` and have
-  // it store one integer per event using the `itype` defined above.
-  auto itype = H5::PredType::NATIVE_INT;
-  OneDimBuffer<int> int_buffer(file, "ints", itype, buffer_size);
   // The more complicated buffer stores entries of type `entryType`
   // from above.
   auto entryType = getEntryType();
@@ -150,28 +121,16 @@ int main(int argc, char* argv[]) {
   // Part 4: generate some bogus data
 
   for (int iii = 0; iii < 500; iii++) {
-    // the int_buffer is easy: just push back.
-    int_buffer.push_back(iii);
 
     // Create an `Entry` object and fill it with garbage data.
     Entry entry;
-    entry.value_d = std::sqrt(iii);
-    entry.value_i = iii;
-    entry.value_s = "this is " + std::to_string(iii);
     for (int jjj = 0; jjj < (iii % 10); jjj++) {
-      entry.vector_d.push_back(jjj / double(iii + 1));
-      entry.vector_s.push_back(std::to_string(jjj));
-      entry.tracks.push_back({100.0*jjj, std::sin(jjj)});
-      h5::vector<int> iv;
       MultiTrack tk;
       for (int kkk = 0; kkk < jjj; kkk++) {
-    	iv.push_back(kkk);
 	tk.tracks.push_back({100.0*jjj, std::sin(jjj)});
 	tk.n_tracks = jjj;
       }
-      entry.vv_i.push_back(iv);
       entry.verts.push_back(tk);
-
     }
 
     // add the `Entry` to the buffer.
@@ -179,7 +138,6 @@ int main(int argc, char* argv[]) {
   }
   // Buffers need to be flushed after the loop, since `flush` is only
   // called automatically when the buffer fills up.
-  int_buffer.flush();
   ebuffer.flush();
   return 0;
 }
